f9df302e2ac1377663d7a4bffec94ebe128b0962
Giedrius Trumpickas <trumpyla@gmail.com>
2023-05-05 17:01:20 -0400

    Revert "Added protobuf tap"
    This reverts commit 32852dcf18c414f64a6efa61061769f1f9c4a4c2.

--- protobuf241.rb
+++ protobuf241.rb
@@ -1,4 +1,4 @@
-class Protobuf < Formula
+class Protobuf241 < Formula
   homepage "https://github.com/google/protobuf"
   url "https://github.com/google/protobuf/releases/download/v2.4.1/protobuf-2.4.1.tar.bz2"
   sha256 "cf8452347330834bbf9c65c2e68b5562ba10c95fa40d4f7ec0d2cb332674b0bf"
@@ -11,74 +11,49 @@ class Protobuf < Formula
 
   keg_only "Conflicts with protobuf in main repository."
 
-  patch :DATA
 
-  depends_on "cmake" => :build
-  depends_on "python@3.10" => [:build, :test]
-  depends_on "python@3.11" => [:build, :test]
 
-  uses_from_macos "zlib"
-
-  def pythons
-    deps.map(&:to_formula)
-        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
-        .map { |f| f.opt_libexec/"bin/python" }
+  fails_with :llvm do
+    build 2334
   end
 
+  # Fix build with clang and libc++
+  patch :DATA
+
   def install
-    cmake_args = %w[
-      -Dprotobuf_BUILD_LIBPROTOC=ON
-      -Dprotobuf_INSTALL_EXAMPLES=ON
-      -Dprotobuf_BUILD_TESTS=OFF
-    ] + std_cmake_args
+    # Don't build in debug mode. See:
+    # https://github.com/homebrew/homebrew/issues/9279
+    ENV.prepend "CXXFLAGS", "-DNDEBUG"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--with-zlib"
+    system "make"
+    system "make", "install"
 
-    system "cmake", "-S", ".", "-B", "build", "-Dprotobuf_BUILD_SHARED_LIBS=ON", *cmake_args
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
+    # Install editor support and examples
+    doc.install "editors", "examples"
+  end
 
-    pkgshare.install "editors/proto.vim"
-    elisp.install "editors/protobuf-mode.el"
-
-    ENV.append_to_cflags "-I#{include}"
-    ENV.append_to_cflags "-L#{lib}"
-    ENV["PROTOC"] = bin/"protoc"
-
-    cd "python" do
-      pythons.each do |python|
-        pyext_dir = prefix/Language::Python.site_packages(python)/"google/protobuf/pyext"
-        with_env(LDFLAGS: "-Wl,-rpath,#{rpath(source: pyext_dir)} #{ENV.ldflags}".strip) do
-          system python, *Language::Python.setup_install_args(prefix, python), "--cpp_implementation"
-        end
-      end
-    end
-
-    system "cmake", "-S", ".", "-B", "static",
-           "-Dprotobuf_BUILD_SHARED_LIBS=OFF",
-           "-DWITH_PROTOC=#{bin}/protoc",
-           *cmake_args
-    system "cmake", "--build", "static"
-    lib.install buildpath.glob("static/*.a")
+  def caveats; <<-EOS
+    Editor support and examples have been installed to:
+      #{doc}
+    EOS
   end
 
   test do
-    testdata = <<~EOS
-      syntax = "proto3";
+    (testpath/"test.proto").write <<-EOS.undent
       package test;
       message TestCase {
-        string name = 4;
+        required string name = 4;
       }
       message Test {
         repeated TestCase case = 1;
       }
     EOS
-    (testpath/"test.proto").write testdata
-    system bin/"protoc", "test.proto", "--cpp_out=."
 
-    pythons.each do |python|
-      system python, "-c", "import google.protobuf"
-    end
+    system bin/"protoc", "test.proto", "--cpp_out=."
   end
 end
+
 __END__
 diff --git a/src/google/protobuf/message.cc b/src/google/protobuf/message.cc
 index 91e6878..0409a94 100644
